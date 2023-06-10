eff100b32da31c3755f34a98f250b111d34d322b
Leandro Cardozo <50833557+leanczo@users.noreply.github.com>
2023-06-10 20:20:41 -0300

    ✨ updated to testimonials

--- src/components/TestimonialCard/TestimonialCard.js
+++ src/components/TestimonialCard/TestimonialCard.js
@@ -1,10 +1,22 @@
 import React from "react";
 import "./TestimonialCard.css";
+import starIcon from "../../assets/star.svg";
 
 const TestimonialCard = ({ author, description, image, rating }) => {
   return (
     <div className="testimonial-card">
-       
+      <div className="testimonial-rating">
+        {rating &&
+          [...Array(rating)].map((_, index) => (
+            <img key={index} src={starIcon} alt="star" height={15} width={15} />
+          ))}
+      </div>
+
+      <div className="testimonial-card-container">
+        <img className="testimonial-profile-picture" src={image} alt="author" height={50}></img>
+        <p className="testimonial-card-author">{author}</p>
+      </div>
+      <p className="testimonial-description">{description}</p>
     </div>
   );
 };
--- src/pages/Sections/Testimonials/Testimonials.js
+++ src/pages/Sections/Testimonials/Testimonials.js
@@ -4,52 +4,51 @@ import TestimonialCard from "../../../components/TestimonialCard/TestimonialCard
 
 const testimonials = [
   {
-    author: "Greek salad",
+    author: "Isabella Montgomery",
     description:
-      "The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. ",
+      "Little Lemon's Bruschetta is a symphony of flavors; perfectly crispy bread coupled with the freshest tomatoes.",
     image:
-      "https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/08/Greek-Salad-main.jpg",
-    rating: "$12.99",
+      "https://images.pexels.com/photos/3763188/pexels-photo-3763188.jpeg?cs=srgb&dl=pexels-andrea-piacquadio-3763188.jpg&fm=jpg",
+    rating: 5,
   },
   {
-    author: "Bruschetta",
+    author: "Alexander Thompson",
     description:
-      "Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.",
+      "The burger at Little Lemon was a taste revelation with its juicy, perfectly cooked meat and harmonious ingredient pairing.",
     image:
-      "https://hips.hearstapps.com/hmg-prod/images/bruschetta-index-645d03e6f174d.jpg?crop=0.8890414878397711xw:1xh;center,top&resize=1200:*",
-    rating: "$ 5.99",
+      "https://www.irreverentgent.com/wp-content/uploads/2018/03/Awesome-Profile-Pictures-for-Guys-look-away2.jpg",
+    rating: 5,
   },
   {
-    author: "Lemon Dessert",
+    author: "Olivia Jenkins",
     description:
-      "This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined.",
+      "The Greek Salad at Little Lemon was delightfully fresh and flavorful, with a high-quality dressing. ",
     image:
-      "https://www.southernliving.com/thmb/z706skTv8rLcnFwyTEnMzr_H5zQ=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Southern-Living_27364_SR_Lemon-Lush_13190-fd96c709fc2946a7aeb5c869f9ad470e.jpg",
-    rating: "$ 5.00",
+      "https://images.pexels.com/photos/762020/pexels-photo-762020.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    rating: 4,
   },
 ];
 
 const Testimonials = () => {
   return (
     <div className="testimonial-background">
-        <div className="testimonials">
-      <div className="testimonials-container">
-        <h1 className="testimonial-title">Testimonials</h1>
-      </div>
+      <div className="testimonials">
+        <div className="testimonials-container">
+          <h1 className="testimonial-title">Testimonials</h1>
+        </div>
 
-      <div className="testimonials-list">
-        {testimonials.map((special) => (
-          <TestimonialCard
-            title={special.title}
-            description={special.description}
-            image={special.image}
-            price={special.price}
-          />
-        ))}
+        <div className="testimonials-list">
+          {testimonials.map((testimonial) => (
+            <TestimonialCard
+              author={testimonial.author}
+              description={testimonial.description}
+              image={testimonial.image}
+              rating={testimonial.rating}
+            />
+          ))}
+        </div>
       </div>
     </div>
-    </div>
-    
   );
 };
 
