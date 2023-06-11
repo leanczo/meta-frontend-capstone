7cdfb0c1c87fddfbf532e0ce1adfa0fc23ad9a3e
Leandro Cardozo <50833557+leanczo@users.noreply.github.com>
2023-06-11 18:07:01 -0300

    🔨 add mobile support

--- src/layouts/Topbar/Topbar.js
+++ src/layouts/Topbar/Topbar.js
@@ -1,53 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Topbar.css";
 import Logo from "../../assets/Logo.svg";
+import MenuIcon from "../../assets/hamburger.svg";
 import { useNavigate } from "react-router-dom";
+
 const Topbar = () => {
   const navigate = useNavigate();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   const onClickImage = () => {
     navigate("/");
   };
+
+  const menuItems = [
+    { id: 1, label: "Home", link: "/" },
+    { id: 2, label: "About", link: "/about" },
+    { id: 3, label: "Menu", link: "/menu" },
+    { id: 4, label: "Reservations", link: "/reservations" },
+    { id: 5, label: "Order Online", link: "/orders" },
+    { id: 6, label: "Login", link: "/login" }
+  ];
 
   return (
     <header className="topbar">
       <div className="logo">
         <img src={Logo} alt="logo" height={50} onClick={onClickImage} />
       </div>
-      <nav className="nav">
-        <ul>
-          <li>
-            <a href="/" className="link">
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="/about" className="link">
-              About
-            </a>
-          </li>
-          <li>
-            <a href="/menu" className="link">
-              Menu
-            </a>
-          </li>
-          <li>
-            <a href="/reservations" className="link">
-              Reservations
-            </a>
-          </li>
-          <li>
-            <a href="/orders" className="link">
-              Order Online
-            </a>
-          </li>
-          <li>
-            <a href="/login" className="link">
-              Login
-            </a>
-          </li>
+      <nav className={`nav ${isMenuOpen ? "open" : ""}`}>
+        <ul className={`menu-list ${isMenuOpen ? "open" : ""}`}>
+          {menuItems.map(item => (
+            <li key={item.id}>
+              <a href={item.link} className="link">
+                {item.label}
+              </a>
+            </li>
+          ))}
         </ul>
       </nav>
+      <div className="menu-toggle" onClick={toggleMenu}>
+        <img src={MenuIcon} alt="menu-icon" />
+      </div>
     </header>
   );
 };
